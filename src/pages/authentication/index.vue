4551015e74dba83297099314ecfa411645f3c3bf
Nikita Ignatovich <nikita@ignatovich.me>
2024-04-25 00:20:45 +0300

    add style for authentication layout

--- src/pages/authentication/index.vue
+++ src/pages/authentication/index.vue
@@ -1,7 +1,24 @@
 <script setup lang="ts"></script>
 
 <template>
-  <RouterView></RouterView>
+  <main :class="$style['page-authentication']">
+    <div :class="$style['page-authentication__wrapper']">
+      <RouterView></RouterView>
+    </div>
+  </main>
 </template>
 
-<style></style>
+<style module lang="scss">
+.page-authentication {
+  width: 100dvw;
+  height: 100dvh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &__wrapper {
+    min-width: 350px;
+    max-width: calc(100dvw - 20px);
+  }
+}
+</style>
