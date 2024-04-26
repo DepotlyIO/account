a9f3b82066815e1cf7c1f9361f9d72978bbf919f
Nikita Ignatovich <nikita@ignatovich.me>
2024-04-26 20:43:59 +0300

    add unhead package

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,20 @@
 import '@/assets/styles/main.scss';
 
 import { createApp } from 'vue';
-import { createPinia } from 'pinia';
 
 import App from '@/app.vue';
+
+import pinia from '@/configurations/pinia';
 import router from '@/configurations/router';
 import i18n from '@/configurations/i18n';
+import unhead from '@/configurations/unhead';
+
 import { useUserStore } from '@/stores/user';
 
-const app = createApp(App).use(createPinia());
+const app = createApp(App).use(pinia);
 
 const userStore = useUserStore();
 
 userStore.getUserInfo().then(() => {
-  app.use(router).use(i18n).mount('#app');
+  app.use(router).use(i18n).use(unhead).mount('#app');
 });
