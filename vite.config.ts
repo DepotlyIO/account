1f051d6bfba0844d5bee88c5161a00bb44e8ff53
Nikita Ignatovich <nikita@ignatovich.me>
2024-04-26 19:29:29 +0300

    add proxy for development

--- vite.config.ts
+++ vite.config.ts
@@ -31,4 +31,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  server: {
+    proxy: {
+      '/v1': 'https://api.depotly.io',
+    },
+  },
 });
