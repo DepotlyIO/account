89b78a1701bdba4054d351324e915421ca02307a
Nikita Ignatovich <nikita@ignatovich.me>
2024-05-01 22:18:05 +0300

    new icons

--- src/types/assets/icons.ts
+++ src/types/assets/icons.ts
@@ -2,4 +2,11 @@ export type Sprite = 'main';
 
 export type Icon = MainIcon;
 
-export type MainIcon = 'account-circle' | 'chart-bar' | 'home' | 'loading' | 'wallet-bifold';
+export type MainIcon =
+  | 'account-circle'
+  | 'chart-bar'
+  | 'close'
+  | 'home'
+  | 'loading'
+  | 'update'
+  | 'wallet-bifold';
