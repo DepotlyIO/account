2f783a1069f2e4b082792aff99ba112cce6f173c
Nikita Ignatovich <nikita@ignatovich.me>
2024-05-12 12:57:29 +0300

    rename file

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 import VueDevTools from 'vite-plugin-vue-devtools';
 import { VitePWA } from 'vite-plugin-pwa';
 
-import getCssModulesNamesGenerator from './utils/cssModulesNamesGenerator';
+import getCssModulesNamesGenerator from './utils/css-modules-names-generator';
 
 // https://vitejs.dev/config/
 export default defineConfig({
