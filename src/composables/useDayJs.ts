c0801e68867352ec699cca4083e00d74ef914526
Nikita Ignatovich <nikita@ignatovich.me>
2024-05-26 20:31:30 +0300

    extend dayjs

--- src/composables/useDayJs.ts
+++ src/composables/useDayJs.ts
@@ -1,6 +1,12 @@
 import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+import 'dayjs/locale/en';
 
 dayjs.extend(duration);
+dayjs.extend(relativeTime);
+
+dayjs.locale('en');
 
 export const useDayJs = () => dayjs;
