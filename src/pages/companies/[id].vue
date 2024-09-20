095a5a4ec18a22699d2ffab8d8e0b87786b4a5be
Nikita Ignatovich <nikita@ignatovich.me>
2024-09-20 15:04:56 +0300

    base loading logic for company page

--- src/pages/companies/[id].vue
+++ src/pages/companies/[id].vue
@@ -1,7 +1,46 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useRoute } from 'vue-router';
+import { useHead } from '@unhead/vue';
+import { useApi } from '@/composables/useApi';
+import UiText from '@/components/ui/Text.vue';
+import type { Company } from '@/types/models/company';
+
+const route = useRoute();
+const api = useApi();
+
+const loading = ref(false);
+const company = ref<Company>();
+
+const loadCompany = async () => {
+  if (loading.value) return;
+
+  loading.value = true;
+  try {
+    const { data } = await api.companies.one(route.params.id);
+    company.value = data;
+  } catch (e) {
+    console.error(e);
+  }
+  loading.value = false;
+};
+
+loadCompany();
+
+useHead(() => ({
+  title: company.value?.name,
+}));
+</script>
 
 <template>
-  <div>Company</div>
+  <div :class="$style['page-companies-id']">
+    <UiText variant="h1">
+      {{ company?.name }}
+    </UiText>
+  </div>
 </template>
 
-<style module lang="scss"></style>
+<style module lang="scss">
+.page-companies-id {
+}
+</style>
